82466f2aa2165fe06a421e333aa10a56492d7c47
Dmitriy Danilin <danilindn@icloud.com>
2022-02-18 14:31:52 +0300

    Initial Commit

---
+++ TinkoffChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Дмитрий Данилин on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
