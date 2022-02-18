8871ef27a658e226c1aaed86e11593e31551b758
Dmitriy Danilin <danilindn@icloud.com>
2022-02-18 17:39:55 +0300

    Жизненный цикл UIViewController

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -11,9 +11,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("View has been loaded into memory: \(#function)")
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("View will be added to the hierarchy from memory: \(#function)")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("View is getting ready to resize itself to fit the screen: \(#function)")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("View resized to fit the screen: \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("View added to hierarchy and displayed: \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("View is preparing to be removed from the hierarchy: \(#function)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("View has been removed from the hierarchy and is not displayed: \(#function)")
+    }
 
 }
 
