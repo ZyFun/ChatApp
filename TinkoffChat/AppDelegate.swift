1c06afabebf7f25f2740bc070f227e266c759146
Dmitriy Danilin <danilindn@icloud.com>
2022-03-04 13:45:11 +0300

    Добавил контроллер ConversationsListViewController
    Сделал точку входа кодом через AppDelegate

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -12,9 +12,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         
+        createAndShowStartVC()
         return true
+    }
+}
+
+// MARK: - Initial application settings
+private extension AppDelegate {
+    func createAndShowStartVC() {
+        let conversationsListVC = ConversationsListViewController(
+            nibName: String(describing: ConversationsListViewController.self),
+            bundle: nil
+        )
+        
+        let navigationController = UINavigationController(
+            rootViewController: conversationsListVC
+        )
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 }
 
