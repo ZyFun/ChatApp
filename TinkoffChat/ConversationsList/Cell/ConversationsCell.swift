70689a6eb7da31013e356ad58435d0db7e042661
Dmitriy Danilin <danilindn@icloud.com>
2022-03-04 16:08:10 +0300

    Подготовил макет ячейки

---
+++ TinkoffChat/ConversationsList/Cell/ConversationsCell.swift
@@ -0,0 +1,23 @@
+//
+//  ConversationsCell.swift
+//  TinkoffChat
+//
+//  Created by Дмитрий Данилин on 04.03.2022.
+//
+
+import UIKit
+
+final class ConversationsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
