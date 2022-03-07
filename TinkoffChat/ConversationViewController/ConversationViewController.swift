907bda403963be078098997cff3fecbcb4ce9b71
Dmitriy Danilin <danilindn@icloud.com>
2022-03-07 14:20:00 +0300

    Подготовил экран для беседы и переместил модельку

---
+++ TinkoffChat/ConversationViewController/ConversationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ConversationViewController.swift
+//  TinkoffChat
+//
+//  Created by Дмитрий Данилин on 07.03.2022.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
