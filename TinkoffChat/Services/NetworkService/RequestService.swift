d8e1cca2b7248e0459127150adb19a453f591d07
Dmitriy Danilin <danilindn@icloud.com>
2022-04-28 17:34:51 +0300

    Разделил сетевой слой на разные файлы

--- TinkoffChat/Services/NetworkService/RequestService.swift
+++ TinkoffChat/Services/NetworkService/RequestService.swift
@@ -1,5 +1,5 @@
 //
-//  NetworkService.swift
+//  RequestService.swift
 //  TinkoffChat
 //
 //  Created by Дмитрий Данилин on 24.04.2022.
@@ -78,55 +78,5 @@ class RequestSender: IRequestSenderProtocol {
             }
         }
         task.resume()
-    }
-}
-
-class PixabayParser: IParserProtocol {
-    typealias Model = PixabayApiModel
-    
-    func parse(data: Data) -> Model? {
-        var model: Model?
-        do {
-            model = try JSONDecoder().decode(Model.self, from: data)
-        } catch {
-            Logger.error(error.localizedDescription)
-        }
-        return model
-    }
-}
-
-struct PixabayRequest: IRequestProtocol {
-    var urlRequest: URLRequest?
-    var urlString: String
-    
-    init(urlString: String) {
-        self.urlString = urlString
-        urlRequest = request(stringURL: urlString)
-    }
-    
-    mutating func request(stringURL: String) -> URLRequest? {
-        if let url = URL(string: stringURL) {
-            urlRequest = URLRequest(url: url, timeoutInterval: 30)
-        } else {
-            Logger.error("Неправильный URL")
-        }
-        return urlRequest
-    }
-}
-
-struct RequestFactory {
-    struct PixabayPhotoRequest {
-        static func modelConfig() -> RequestConfig<PixabayParser> {
-            let token = TokenProvider.pixabayTokenAPI
-            let urlRequest = URLProvider.fetchApiPixabayStringURL(with: token)
-            let request = PixabayRequest(urlString: urlRequest)
-            let parser = PixabayParser()
-            return RequestConfig<PixabayParser>(request: request, parser: parser)
-        }
-        
-        static func imagePixabayConfig(from urlString: String) -> RequestConfig<PixabayParser> {
-            let request = PixabayRequest(urlString: urlString)
-            return RequestConfig<PixabayParser>(request: request, parser: nil)
-        }
     }
 }
