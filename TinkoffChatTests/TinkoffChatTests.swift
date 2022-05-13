941392a9cdd0977f695484d036facc69379fbb45
Dmitriy Danilin <danilindn@icloud.com>
2022-05-13 17:54:18 +0300

    Добавил таргет для тестов
    Поправил небольшой баг с firebase при тестировании.

---
+++ TinkoffChatTests/TinkoffChatTests.swift
@@ -0,0 +1,13 @@
+//
+//  TinkoffChatTests.swift
+//  TinkoffChatTests
+//
+//  Created by Дмитрий Данилин on 13.05.2022.
+//
+
+import XCTest
+@testable import TinkoffChat
+
+class TinkoffChatTests: XCTestCase {
+    
+}
